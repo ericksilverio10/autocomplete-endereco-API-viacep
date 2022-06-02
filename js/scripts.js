8ac7cf8eaa6691ebaea8ca86d7a72e7673cb6f7f
Erick <erick1junior@hotmail.com>
2022-06-02 10:44:36 -0300

    Resgatando elementos no script

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
-console.log("Hello world");
+const addressForm = document.querySelector("#address-form");
+const cepInput = document.querySelector("#cep");
+const addressInput = document.querySelector("#address");
+const cityInput = document.querySelector("#city");
+const neighborhoodInput = document.querySelector("#neighborhood");
+const regionInput = document.querySelector("#region");
+const formInputs = document.querySelectorAll("[data-input]");
+
+const closeButton = document.querySelector("#close-message");
+
+//Validade CEP input
+cepInput.addEventListener("keypress",(e) =>{
+    const onlyNumbers = /[0-9]/;
+    const key = String.fromCharCode(e.keyCode);
+
+    console.log(e.keyCode);
+    console.log(key);
+
+    //allow only numbers
+    if(!onlyNumbers.test(key)){
+        e.preventDefault();
+        return
+    }
+})
