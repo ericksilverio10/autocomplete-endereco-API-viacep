61260856b32d4b237b4f4cc960d71c4a90109e92
Erick <erick1junior@hotmail.com>
2022-06-02 15:32:33 -0300

    Finalizando script

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ const cityInput = document.querySelector("#city");
 const neighborhoodInput = document.querySelector("#neighborhood");
 const regionInput = document.querySelector("#region");
 const formInputs = document.querySelectorAll("[data-input]");
+const fadeElement = document.querySelector("#fade");
 
 const closeButton = document.querySelector("#close-message");
 
@@ -13,12 +14,113 @@ cepInput.addEventListener("keypress",(e) =>{
     const onlyNumbers = /[0-9]/;
     const key = String.fromCharCode(e.keyCode);
 
-    console.log(e.keyCode);
-    console.log(key);
-
     //allow only numbers
     if(!onlyNumbers.test(key)){
         e.preventDefault();
         return
     }
+})
+
+//Get address event
+cepInput.addEventListener("keyup", (e)=>{
+    const inputValue = e.target.value;
+
+    //console.log(inputValue);
+    
+    //Check if we have the correct length
+    if(inputValue.length === 8){
+        getAddress(inputValue);
+    }
+});
+
+//Get costumer address from API
+const getAddress = async(cep)=>{
+    console.log(cep);
+    toggleLoader();
+
+    cepInput.blur();
+
+    const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
+    const response = await fetch(apiUrl);
+
+    const data = await response.json();
+
+    //Show error and reset form
+    if(data.erro === "true"){
+        if(!addressInput.hasAttribute("disabled")){
+            toggleDisabled();
+        }
+        addressForm.reset();
+        toggleLoader();
+        toggleMessage("CEP Inválido, tente novamente");
+        return;
+    }
+
+    if(addressInput.value === ""){
+        toggleDisabled();
+    };
+    
+    console.log(data);
+
+    addressInput.value = data.logradouro;
+    cityInput.value = data.localidade;
+    neighborhoodInput.value = data.bairro;
+    regionInput.value = data.uf;
+
+    toggleLoader();
+};
+
+//Add or remove disabled attribute
+const toggleDisabled =() =>{
+    if(regionInput.hasAttribute("disabled")){
+        formInputs.forEach((input) => {
+            input.removeAttribute("disabled");
+        })
+    } else{
+        formInputs.forEach((input) => {
+            input.setAttribute("disabled","disabled");
+        }) 
+    }
+}
+
+//Show or hide loader
+const toggleLoader = () =>{
+    
+    const loaderElement = document.querySelector("#loader");
+
+    fadeElement.classList.toggle("hide");
+    loaderElement.classList.toggle("hide");
+}
+
+//Show or hide message
+const toggleMessage = (msg) =>{
+    const messageElement = document.querySelector("#message");
+    const messageElementText = document.querySelector("#message p");
+
+    messageElementText.innerText = msg;
+    
+    fadeElement.classList.toggle("hide");
+    messageElement.classList.toggle("hide");
+};
+
+function hideMessage() {
+    const messageElement = document.querySelector("#message");
+    fadeElement.classList.add("hide");
+    messageElement.classList.add("hide");
+    cepInput.focus();
+}
+
+//Save address
+addressForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    toggleLoader();
+    setTimeout(()=>{
+        toggleLoader();
+        toggleMessage("Endereço salvo com sucesso!");
+
+        addressForm.reset();
+
+        toggleDisabled();
+    }, 1500)
 })
